28b62f3e189b2006bd9d39532ca99f6b0961f7c6
Neale Taylor <nealeptaylor@gmail.com>
2022-06-23 14:36:27 -0400

    more practice examples

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-ReactDOM.render(
-  <ul><li>Number 1</li><li>Number 2</li></ul>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <ul><li>Number 1</li><li>Number 2</li></ul>,
+//   document.getElementById("root")
+// )
+
+const navbar = (
+  <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+      <li>Menu</li>
+      <li>Pricing</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 )
+
+ReactDOM.render(navbar, document.getElementById("root"))
