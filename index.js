4ce33b4d84a9f1b6178c32fa85d6dd9d3fa42afa
Neale Taylor <nealeptaylor@gmail.com>
2022-06-23 14:12:22 -0400

    add another react render

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(<ul><li>Number 1</li><li>Number 2</li></ul> , document.getElementById("root"))
